26710ba664d934ceae3474f21ec954c5b5b91249
Mohit Marwaha <mohitmarwaha98@gmail.com>
2025-01-09 01:42:01 -0800

    main.py: More easier way and improvements in code

--- main.py
+++ main.py
@@ -1,35 +1,25 @@
 import random
 
 # Randomly select a choice for the computer
-computer = random.choice([1,0,2])
+computer = random.choice(["snake","water","gun"])
 
 # Mapping user input to game choices
-youDict = {"s":0, "w":1, "g":2}
-revDict = {0:"Snake", 1:"Water", 2:"Gun"}
+choices = {"s":"snake", "w":"water", "g":"gun"}
 
 # User input with validation
-youChoice = input("Enter your choice 's' for Snake, 'w' for Water, 'g; for Gun: ").lower()
-if youChoice not in youDict:
+youChoice = input("Enter your choice 's' for Snake, 'w' for Water, 'g' for Gun: ").lower()
+
+if youChoice not in choices:
     print("Invalid Choice")
 else:
-    you = youDict[youChoice]
-    print("You chose",revDict[you],"\nComputer Chose",revDict[computer])
+    you = choices[youChoice]
+    print("You chose",you,"\nComputer Chose",computer)
 
     #Logic
     if computer == you:
         print("Its a draw")
     else:
-        if computer == 0 and you == 1:
-            print("You Lose")
-        elif computer == 0 and you == 2:
-            print("You Won")
-        elif computer == 1 and you == 0:
-            print("You Won")
-        elif computer == 1 and you == 2:
-            print("You Lose")
-        elif computer == 2 and you == 0:
-            print("You Lose")
-        elif computer == 2 and you == 1:
+        if you == "snake" and computer == "water" or you == "water" and computer == "gun" or you == "gun" and computer == "snake":
             print("You Win")
         else:
-            print("Something went wrong")
+            print("You Lose")
